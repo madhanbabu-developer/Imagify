3c1efd0f0d9c6614fc7a9bf39486f94de029f529
madhanbabu-developer <madhanbabu1706@gmail.com>
2025-09-28 18:23:17 +0530

    Add files via upload

---
+++ middlewares/auth.js
@@ -0,0 +1,32 @@
+
+import jwt from 'jsonwebtoken';
+
+const userAuth = async (req, res, next) => {
+  const { token } = req.headers;
+
+  if (!token) {
+    return res.json({
+      success: false,
+      message: "Not Authorized. Login Again",
+    });
+  }
+
+  try {
+    const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
+
+    if (tokenDecode.id) {
+      req.userId = tokenDecode.id; // attach userId to request
+      next();
+    } else {
+      return res.json({
+        success: false,
+        message: "Not Authorized. Login Again",
+      });
+    }
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export default userAuth;
+
